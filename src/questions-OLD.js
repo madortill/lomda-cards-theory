27c8ec9eb3b1c38afa26ebd89248a6ba63841451
tomy <poli.tomy21@gmail.com>
2021-12-13 19:34:39 +0200

    rename

---
+++ src/questions-OLD.js
@@ -0,0 +1,86 @@
+/**
+ * שני סוגים של שאלות: 
+ * 
+ * שאלות אמריקאיות: 
+    {
+        type: "multiple",
+        img: `../assets/images/image_example.jpg`,
+        question: `11111 road`,
+        ans1: `תשובה`,
+        ans2: `תשובה`,
+        ans3: `תשובה`,
+        ans4: `תשובה`,
+        correctAns: "ans1"
+    },
+ * 
+ * שאלות נכון לא נכון:
+ * 
+    {
+        type: "binary",
+        sentence: `דוגמה למשפט`,
+        trueOrFalse: true
+    }
+ */
+
+
+const DATA = { 
+    "חוק ותמרורים": {
+        "questions": [
+            {
+                type: "multiple",
+                img: `../assets/images/image_example.jpg`,
+                question: `11111 road`,
+                ans1: `c`,
+                ans2: `c`,
+                ans3: `c`,
+                ans4: `c`,
+                correctAns: "ans1"
+            },
+        ],
+    },
+    
+    "בטיחות": {
+        "questions": [
+            {
+                type: "multiple",
+                img: `../assets/images/image_example.jpg`,
+                question: `11111 road`,
+                ans1: `c`,
+                ans2: `c`,
+                ans3: `c`,
+                ans4: `c`,
+                correctAns: "ans1"
+            },
+        ],
+    },
+
+    "הכרת הרכב": {
+        "questions": [
+            {
+                type: "multiple",
+                img: `../assets/images/image_example.jpg`,
+                question: `11111 road`,
+                ans1: `c`,
+                ans2: `c`,
+                ans3: `c`,
+                ans4: `c`,
+                correctAns: "ans1"
+            },
+        ],
+    },
+    
+    "רכב משא": {
+        "questions": [
+            {
+                type: "multiple",
+                img: `../assets/images/image_example.jpg`,
+                question: `11111 road`,
+                ans1: `c`,
+                ans2: `c`,
+                ans3: `c`,
+                ans4: `c`,
+                correctAns: "ans1"
+            },
+        ],
+    },
+}
